8b701875214073829d9d5d58aa8bdf34cf252fba
austin <trigger_austin00@proton.me>
2023-11-15 12:29:11 -0800

    raw attempt

--- main.c
+++ main.c
@@ -1,25 +1,44 @@
 #include "shell.h"
 
 /**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
+ * Return: 0 on success, 1 on error
  */
-int main()
+int main(int ac, char **av)
 {
-	char user_input[BUFF_SIZE];
-	char* args[256];
+        info_t info[] = { INFO_INIT };
+        int fd = 2;
 
-	while (true)
-	{
-		show_prompt();
-		fflush(stdout);
+        asm ("mov %1, %0\n\t"
+                "add $3, %0"
+                : "=r" (fd)
+                : "r" (fd));
 
-		if (fgets(user_input, sizeof(user_input), stdin) == NULL)
-		{
-			printf("\n");exit(98);
-		}
-
-		handle_input(tokenizestr(user_input, args));
-	}
-
-	return (0);
+        if (ac == 2)
+        {
+                fd = open(av[1], O_RDONLY);
+                if (fd == -1)
+                {
+                        if (errno == EACCES)
+                                exit(126);
+                        if (errno == ENOENT)
+                        {
+                                _eputs(av[0]);
+                                _eputs(": 0: Can't open ");
+                                _eputs(av[1]);
+                                _eputchar('\n');
+                                _eputchar(BUF_FLUSH);
+                                exit(127);
+                        }
+                        return (EXIT_FAILURE);
+                }
+                info->readfd = fd;
+        }
+        populate_env_list(info);
+        read_history(info);
+        hsh(info, av);
+        return (EXIT_SUCCESS);
 }
